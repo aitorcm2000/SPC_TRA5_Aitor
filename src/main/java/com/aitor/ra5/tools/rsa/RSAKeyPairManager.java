9fbac333cf089a249bd8e7d38baf22156979b90a
aitor <aitor@aitor-laptop>
2025-01-30 13:35:56 +0100

    Started RSA and took notes

--- src/main/java/com/aitor/ra5/tools/rsa/RSAKeyPairManager.java
+++ src/main/java/com/aitor/ra5/tools/rsa/RSAKeyPairManager.java
@@ -4,10 +4,20 @@
  */
 package com.aitor.ra5.tools.rsa;
 
+import java.io.File;
+
 /**
  *
  * @author aitor
  */
-public class RSAKeyPairManager {
-    
+public class RSAKeyPairManager extends RSAKeyPairGenerator {
+//    private final String PATH = ".RSAkeys";
+//    
+//    public int genKeyFileGen(String nombre){
+//        File publicKeyFile = new File(PATH+nombre+".pub");
+//        File privateKeyFile = new File(PATH+nombre+".sec");
+//        
+//        if (publicKeyFile)
+//        
+//    }
 }
